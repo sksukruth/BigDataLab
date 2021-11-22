d385de2d5c0a95210d4855b28478eb6c9bf8a766
sksukruth <71667976+sksukruth@users.noreply.github.com>
2021-11-22 21:56:52 +0530

    Add files via upload

---
+++ hadoop/prgm1/reducer.java
@@ -0,0 +1,22 @@
+package temp;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.OutputCollector;
+
+public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable>{
+	public void reduce(Text key,Iterator<IntWritable> temperatures,OutputCollector<Text,IntWritable> values,Reporter r)throws IOException{
+		int maxtemp = Integer.MIN_VALUE;
+		while(temperatures.hasNext()){
+			int temp = temperatures.next().get();
+			maxtemp = Math.max(maxtemp,temp);
+		}
+		values.collect(key,new IntWritable(maxtemp));
+	}
+}
