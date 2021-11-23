2ae1f74928ee33b7a47e9c230a683c41888032fe
sksukruth <71667976+sksukruth@users.noreply.github.com>
2021-11-23 11:34:15 +0530

    Add files via upload

---
+++ hadoop/prgm4/driver.java
@@ -0,0 +1,34 @@
+package matrix;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class driver{
+	public static void main(String[] args) throws Exception{
+		if(args.length!=2){
+			System.out.println("Arugements length not proper");
+			System.exit(-1);
+		}
+		Configuration conf = new Configuration();
+		conf.set("m","2");
+		conf.set("n","5");
+		conf.set("p","3");
+		Job job = new Job(conf,"driver");
+		job.setJarByClass(driver.class);
+		job.setMapperClass(mapper.class);
+		job.setReducerClass(reducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job,new Path(args[0]));
+		FileOutputFormat.setOutputPath(job,new Path(args[1]));
+		job.waitForCompletion(true);
+	}
+}
